6a60b4f3b283ea483d5c4b4c71f45a82ec83e93e
Damon Collins <damon.c.p.collins@gmail.com>
2018-11-20 17:22:11 -0500

    added Cash Register

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,32 @@
+class CashRegister
+  attr_reader :discount
+  attr_accessor :total
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @list = []
+    @last_item = 0
+  end
+
+  def add_item(item, price, quantity = 1)
+    quantity.times {|el| @list << item }
+    @last_item = price * quantity
+    @total += @last_item
+  end
+
+  def apply_discount
+    return "There is no discount to apply." if @discount == 0
+    dis = (100 - @discount) / 100.0
+    total = (@total *= dis).to_i
+    "After the discount, the total comes to $#{total}."
+  end
+
+  def items
+    @list
+  end
+
+  def void_last_transaction
+    @total -= @last_item
+  end
+end
